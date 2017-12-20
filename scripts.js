fad0c231e634e68bd91caaf79c7439bf7bcf456c
Magda Balun <magi@op.pl>
2017-12-20 15:17:10 +0100

    Format .js file

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 var url = 'http://api.icndb.com/jokes/random';
 
 var button = document.getElementById('get-joke');
-button.addEventListener('click', function(){
+button.addEventListener('click', function() {
 	getJoke();
 });
 
@@ -9,12 +9,12 @@ var paragraph = document.getElementById('joke');
 
 function getJoke() {
 	var xhr = new XMLHttpRequest();
-  	xhr.open('GET', url);
-  	xhr.addEventListener('load', function(){
-    	var response = JSON.parse(xhr.response);
-    	paragraph.innerHTML = response.value.joke;
-  	});
-  	xhr.send();
+	xhr.open('GET', url);
+	xhr.addEventListener('load', function() {
+		var response = JSON.parse(xhr.response);
+		paragraph.innerHTML = response.value.joke;
+	});
+	xhr.send();
 }
 
 window.addEventListener("load", getJoke());
